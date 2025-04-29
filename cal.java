c69c029df8e2c97266eb43606602232b2c5e0163
komalpawar2k13@gmail.com <komalpawar2k13@gmail.com>
2025-04-29 10:28:39 +0530

    updated

--- cal.java
+++ cal.java
@@ -1,7 +1,7 @@
 public class cal {
     public static void main(String[] args) {
         
-        double num1 = 10;
+        double num1 = 49;
         double num2 = 5;
 
        System.out.println("Addition result :" +(num1+num2));
@@ -9,6 +9,7 @@ public class cal {
        System.out.println("Multiplication result :" +(num1*num2));
        System.out.println("Division result :" +(num1/num2));
 
+      
       
     }
 }
